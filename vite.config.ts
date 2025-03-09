59df5fff56f060770f16f25e390d93521f220704
Yoni Tribber <tribberyoni5@gmail.com>
2025-03-09 23:06:49 +0700

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://api.tribber.live",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+  build: {
+    chunkSizeWarningLimit: 5000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
       },
     },
-  }
+  },
 
 })
